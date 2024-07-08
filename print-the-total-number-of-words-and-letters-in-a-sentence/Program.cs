01cc45f603a572a01ea7dec2f83feddaa93711ea
Mhsnarsln09 <mhsnarsln09@gmail.com>
2024-07-08 13:46:14 +0300

    new tiral print-the-total-number-of-words-and-letters-in-a-sentence

---
+++ print-the-total-number-of-words-and-letters-in-a-sentence/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace print_the_total_number_of_words_and_letters_in_a_sentence;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.Write("Enter a sentence: ");
+        string sentence = Console.ReadLine();
+
+        string[] words = sentence.Split(' ');
+        int wordCount = words.Length;
+
+        int letterCount = 0;
+        foreach (string word in words)
+        {
+            letterCount += word.Length;
+        }
+
+        Console.WriteLine($"Total number of words: {wordCount}");
+        Console.WriteLine($"Total number of letters: {letterCount}");
+    }
+}
