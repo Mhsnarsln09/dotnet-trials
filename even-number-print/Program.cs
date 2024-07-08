965093252c11be734fdbc77dd4556c3c4a7e4d98
Mhsnarsln09 <mhsnarsln09@gmail.com>
2024-07-08 13:30:29 +0300

    first commit

---
+++ even-number-print/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace even_number_print;
+
+class Program
+{
+    static void Main()
+    {
+       
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Enter a positive number: ");
+            int number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+
+        Console.WriteLine("Even numbers:");
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
