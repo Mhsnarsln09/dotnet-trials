dd8f14f653fd307e13d8c2adf54414ec413cbd19
Mhsnarsln09 <mhsnarsln09@gmail.com>
2024-07-08 13:42:59 +0300

    new trial  print-the-words-entered-by-the-user-in-reverse-order

---
+++ print-the-words-entered-by-the-user-in-reverse-order/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace print_the_words_entered_by_the_user_in_reverse_order;
+
+class Program
+{
+    static void Main()
+    {
+         Console.Write("Enter a positive number (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        List<string> words = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+            words.Add(word);
+        }
+
+        Console.WriteLine("Words in reverse order:");
+        words.Reverse();
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
