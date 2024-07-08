7538062bf5777bfca4e238097f3d96222ae5cca6
Mhsnarsln09 <mhsnarsln09@gmail.com>
2024-07-08 13:37:01 +0300

    new trial

---
+++ print-the-numbers-that-are-equal-to-or-divisible-by/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace print_the_numbers_that_are_equal_to_or_divisible_by;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         Console.Write("Enter a positive number (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter a positive number (m): ");
+        int m = int.Parse(Console.ReadLine());
+
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Enter a positive number: ");
+            int number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+
+        Console.WriteLine($"Numbers that are equal to or divisible by {m}:");
+        foreach (int number in numbers)
+        {
+            if (number == m || number % m == 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
