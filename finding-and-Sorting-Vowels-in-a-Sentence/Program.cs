08a8706c74b326d3519cfb1088f88bd5956b6088
Mhsnarsln09 <mhsnarsln09@gmail.com>
2024-07-10 11:40:44 +0300

    finding-and-Sorting-Vowels-in-a-Sentence trial added.

---
+++ finding-and-Sorting-Vowels-in-a-Sentence/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace finding_and_Sorting_Vowels_in_a_Sentence;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a sentence: ");
+            string sentence = Console.ReadLine();
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            List<char> vowelList = new List<char>();
+
+            foreach (char c in sentence)
+            {
+                if (Array.Exists(vowels, vowel => vowel == c))
+                {
+                    vowelList.Add(c);
+                }
+            }
+
+            vowelList.Sort();
+
+            Console.WriteLine("Vowels in the sentence: " + string.Join(", ", vowelList));
+    }
+}
